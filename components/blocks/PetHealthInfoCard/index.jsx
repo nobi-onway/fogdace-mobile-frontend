43eed11189bff88c62e65eb2e2dbaa9e63436c41
Viet Dang <vietdanghoang1705@gmail.com>
2023-10-20 19:31:21 +0700

    add data

--- components/blocks/PetHealthInfoCard/index.jsx
+++ components/blocks/PetHealthInfoCard/index.jsx
@@ -1,7 +1,5 @@
 import React from 'react'
-import { View, Text, StyleSheet } from 'react-native'
-import { MenuOptionCard, SupportItemCard } from '../../elements'
-import { COLORS } from '../../../constants'
+import { View, Text } from 'react-native'
 import Icon3D from '../../elements/Icon3D'
 
 import styles from './style'
