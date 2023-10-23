f851751533288134951694c5a70f8693db095d3a
Viet Dang <vietdanghoang1705@gmail.com>
2023-10-23 20:44:26 +0700

    update add health info card

--- components/blocks/PetInfoSectionCard/index.jsx
+++ components/blocks/PetInfoSectionCard/index.jsx
@@ -1,7 +1,7 @@
-import React from 'react'
-import { View, Text, StyleSheet, Pressable, Image } from 'react-native'
-import { COLORS } from '../../../constants'
+import { View, Text, Pressable } from 'react-native'
 import { Icon2D } from '../../elements'
+
+import styles from './style'
 
 const NoData = () => {
     return(
@@ -15,7 +15,7 @@ const NoData = () => {
     )
 }
 
-const PetInfoSectionCard = ({data}) => {
+const PetInfoSectionCard = ({iconName, title, children=null}) => {
   return (
     <View style={styles.container}>
         <View style={styles.headerContainer}>
@@ -23,94 +23,19 @@ const PetInfoSectionCard = ({data}) => {
                 <View style={styles.iconWrapper}>
                 <Icon2D name="pills" activated/>
                 </View>
-                <Text style={styles.mainText}>Weight</Text>
+                <Text style={styles.mainText}>Cân nặng</Text>
             </View>
-
             <Pressable style={styles.detailWrapper}>
-                <Text style={styles.detailText}>Detail</Text>
+                <Text style={styles.detailText}>Chi tiết</Text>
                 <Icon2D name="right"/>
             </Pressable>
-            
         </View>
 
         <View>
-            <NoData />
+            {children ? children : <NoData />}
         </View>
     </View>
   )
 }
-
-
-const styles = StyleSheet.create({
-    container: {
-     backgroundColor: COLORS.white,
-     paddingHorizontal: 4,
-     paddingVertical: 8,
-     borderRadius: 4
-    },
-    headerContainer: {
-        height: 40,
-        flexDirection: 'row',
-        alignItems: 'center',
-        justifyContent: 'space-between',
-        paddingHorizontal: 8
-    },
-    titleWrapper:{
-        flexDirection: 'row',
-        gap: 4,
-        alignItems: 'center',
-    },
-    detailText:{
-        color: COLORS.lightBlack
-    },
-    mainText:{
-        fontWeight: 'bold'
-    },
-    addBtn:{
-        borderRadius: 4,
-        paddingHorizontal: 14,
-        paddingVertical: 2,
-        borderWidth: 1.5,
-        borderColor: COLORS.tertiary,
-        backgroundColor: COLORS.lightGray,
-        width: "auto",
-        marginHorizontal: 'auto',
-    },
-    detailWrapper:{
-        flexDirection: 'row',
-        alignItems: 'center',
-        gap: 4
-    },
-    addBtnText:{
-        color: COLORS.tertiary,
-        fontWeight: 'bold'
-    },
-    centerHorizontalAndVertical: {
-        flexDirection: 'column',
-        justifyContent: 'center',
-        alignItems: 'center',
-        gap: 8
-    },
-    noDataText: {
-        fontWeight: 'bold',
-        fontSize: 15,
-        color: COLORS.lightBlack
-    },
-    iconWrapper:{
-        backgroundColor: COLORS.info,
-        padding: 4,
-        borderRadius: 4,
-        width: 30
-        
-    },
-    headerWrapper:{
-        flexDirection: 'row',
-        justifyContent: 'space-between'
-    },
-    contentWrapper:{
-        flex: 1
-    }
-
-})
 
 export default PetInfoSectionCard
