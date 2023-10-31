20bdd3381f9bb6cfb2fd0af25f39e03996b4f006
K <thekhoi.k@gmail.com>
2023-11-01 00:14:28 +0700

    move axios post data stripe payment to hook

--- app/(user)/(stacks)/add-profile-pet/pet-list/index.jsx
+++ app/(user)/(stacks)/add-profile-pet/pet-list/index.jsx
@@ -1,10 +1,12 @@
 import { View } from "react-native";
 import { PetList } from "../../../../../components/blocks";
+import { StripePayment } from "../../../../../components/elements";
 
 const PetListAddProfile = () => {
   return (
     <View>
       <PetList />
+      <StripePayment />
     </View>
   );
 };
--- components/elements/StripePaymentButton/index.jsx
+++ components/elements/StripePaymentButton/index.jsx
@@ -1,55 +1,17 @@
-import { StripeProvider, useStripe } from "@stripe/stripe-react-native";
-import axios from "axios";
-import { Alert, Pressable, Text } from "react-native";
-import { BASE_URL } from "../../../constants/url";
+import { StripeProvider } from "@stripe/stripe-react-native";
+import { Pressable, Text } from "react-native";
 import styles from "./style";
+import useStripePayment from "../../../hooks/useStripePayment";
 
 const StripePayment = () => {
   const STRIPE_KEY =
     "pk_test_51O5OO5Fe0KBrb7klJLhfVf8aAPzX4XxIQIB9v0uf76xYVSVEeckEEqAYstvH6ojcvZDoYLLbYgxVbZpdw2ocdkyz00x8xe8elR";
 
-  const { initPaymentSheet, presentPaymentSheet } = useStripe();
-
-  const onCheckout = async () => {
-    // 1. Create a payment intent
-    const response = await axios.post(`${BASE_URL}/payment/intents`, {
-      amount: 12950,
-    });
-    if (response.error) {
-      Alert.alert("Something went wrong");
-      return;
-    }
-
-    // 2. Initialize the Payment sheet
-    const initResponse = await initPaymentSheet({
-      merchantDisplayName: "dev",
-      paymentIntentClientSecret: response.data.metaData,
-      returnURL: `${BASE_URL}/payment/intents`,
-    });
-    if (initResponse.error) {
-      console.log(initResponse.error);
-      Alert.alert("Something went wrong");
-      return;
-    }
-
-    // 3. Present the Payment Sheet from Stripe
-    const paymentResponse = await presentPaymentSheet();
-
-    if (paymentResponse.error) {
-      Alert.alert(
-        `Error code: ${paymentResponse.error.code}`,
-        paymentResponse.error.message
-      );
-      return;
-    }
-
-    // 4. If payment ok -> create the order
-    // onCreateOrder();
-  };
+  const { onCheckout } = useStripePayment();
 
   return (
     <StripeProvider publishableKey={STRIPE_KEY}>
-      <Pressable onPress={onCheckout} style={styles.button}>
+      <Pressable onPress={() => onCheckout({ amount: 123 })} style={styles.button}>
         <Text style={styles.buttonText}>Đặt hàng</Text>
       </Pressable>
     </StripeProvider>
