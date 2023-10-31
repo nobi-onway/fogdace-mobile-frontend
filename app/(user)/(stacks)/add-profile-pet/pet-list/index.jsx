0aa9bd2e1a5b6f759e4d1553985ba5966593d423
K <thekhoi.k@gmail.com>
2023-11-01 00:18:32 +0700

    detach stripe-key to keys.js

--- app/(user)/(stacks)/add-profile-pet/pet-list/index.jsx
+++ app/(user)/(stacks)/add-profile-pet/pet-list/index.jsx
@@ -1,12 +1,10 @@
 import { View } from "react-native";
 import { PetList } from "../../../../../components/blocks";
-import { StripePayment } from "../../../../../components/elements";
 
 const PetListAddProfile = () => {
   return (
     <View>
       <PetList />
-      <StripePayment />
     </View>
   );
 };
--- components/elements/StripePaymentButton/index.jsx
+++ components/elements/StripePaymentButton/index.jsx
@@ -2,16 +2,17 @@ import { StripeProvider } from "@stripe/stripe-react-native";
 import { Pressable, Text } from "react-native";
 import styles from "./style";
 import useStripePayment from "../../../hooks/useStripePayment";
+import { STRIPE_KEY } from "../../../constants/keys";
 
 const StripePayment = () => {
-  const STRIPE_KEY =
-    "pk_test_51O5OO5Fe0KBrb7klJLhfVf8aAPzX4XxIQIB9v0uf76xYVSVEeckEEqAYstvH6ojcvZDoYLLbYgxVbZpdw2ocdkyz00x8xe8elR";
-
   const { onCheckout } = useStripePayment();
 
   return (
     <StripeProvider publishableKey={STRIPE_KEY}>
-      <Pressable onPress={() => onCheckout({ amount: 123 })} style={styles.button}>
+      <Pressable
+        onPress={() => onCheckout({ amount: 123 })}
+        style={styles.button}
+      >
         <Text style={styles.buttonText}>Đặt hàng</Text>
       </Pressable>
     </StripeProvider>
